f69b0147eacdced6c2b42f0748c4e5c03439ce2a
Timur <niktim73431@yandex.ru>
2023-02-02 06:49:57 +0300

    api_final_v6

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import permissions
-
 from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from posts.models import Group, Post, Follow
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
 }
 # Устанавливаем срок жизни токена
 SIMPLE_JWT = {
